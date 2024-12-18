91d3d888fd876c09934bfbaf53dc286275b2c314
Akashkadare <akashkadare30@gmail.com>
2024-12-18 20:51:38 +0530

    initial commit

---
+++ src/main.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import { BrowserRouter } from 'react-router-dom'
+import CoinContextProvider from './context/CoinContext.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <CoinContextProvider>
+        <App />
+      </CoinContextProvider>
+      <div className='light x1'></div>
+  <div className='light x2'></div>
+  <div className='light x3'></div>
+  <div className='light x4'></div>
+  <div className='light x5'></div>
+  <div className='light x6'></div>
+  <div className='light x7'></div>
+  <div className='light x8'></div>
+  <div className='light x9'></div>
+    </BrowserRouter>
+  </React.StrictMode>,
+)
